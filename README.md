5309a25e48a93d49c853cbc79778ce919a44852d
Nick Schmalenberger <nick@schmalenberger.us>
2022-04-06 17:00:45 +0000

    plot code

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 The Google IPv6 statistics at https://www.google.com/intl/en/ipv6/statistics.html appear to have a pattern of higher usage on weekends than weekdays. Maybe caused by lower IPv6 adoption in corporate offices than home or mobile ISPs. In certain periods of the COVID19 pandemic especially in 2020, the weekly minimum gets closer to the maximum, likely due to more people away from the office and working at home. So it is interesting to graph the weekly range, minimum, and maximum values calculated out of the daily values listed in the file https://www.google.com/intl/en_ALL/ipv6/statistics/data/adoption.js and plot these as their own trends.
 
+* !(https://github.com/nschmalenberger/gipv6minmax/blob/master/adoption.svg)
--- adoption.py
+++ adoption.py
@@ -2,7 +2,9 @@ import json
 import collections
 import requests
 import re
+import matplotlib.pyplot as plt
 from datetime import date
+
 
 adoption_js = requests.get('https://www.google.com/intl/en_ALL/ipv6/statistics/data/adoption.js')
 adoption_json = re.search('var googleIPv6AdoptionData = (\[.+\]);', adoption_js.text, re.DOTALL).group(1)
@@ -22,9 +24,22 @@ for day_data in data:
     # each day gets converted to its week and data added to weeks list
     weeks[date.fromisocalendar(year, week, 1)].append(ipv6_traffic)
 
+# initialize lists to plot
+week_plot=list()
+low_plot=list()
+high_plot=list()
+range_plot=list()
 # find the range, high, and low values for each week
 for week, values in weeks.items():
     high=sorted(values)[-1]
     low=sorted(values)[0]
-    range=high-low
-    print("{},{},{},{}".format(week, low, high, range))
+    high_plot.append(high)
+    low_plot.append(low)
+    range_plot.append(high-low)
+    week_plot.append(week)
+
+plt.plot(week_plot, high_plot)
+plt.plot(week_plot, low_plot)
+plt.plot(week_plot, range_plot)
+
+plt.savefig('adoption.svg')
